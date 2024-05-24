209befe2d23200d70d0cdffcfd14c6e2e9a84acc
Nguyễn Ngọc Long <51698746+ZNLong2203@users.noreply.github.com>
2024-05-24 21:02:17 +0700

    add package

--- server/index.js
+++ server/index.js
@@ -3,16 +3,26 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const bodyParser = require('body-parser')
 const passport = require('passport')
+const morgan = require('morgan')
+const helmet = require('helmet')
+const compression = require('compression')
 
+// Importing the routes
 const auth = require('./routes/auth');
 
+// Creating the express app
 const app = express();
-
 dotenv.config()
+
+// Init middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(passport.initialize())
+app.use(morgan('dev')) // HTTP request logger middleware for node.js
+app.use(helmet()) // Secure Express apps by setting various HTTP headers
+app.use(compression()) // Compress all routes to reduce the size of the response body
 
+// Bodyparser middleware
 app.use('/auth', auth)
 
 mongoose.connect(process.env.MONGODB_URL)
