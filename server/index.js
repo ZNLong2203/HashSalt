e4587717af57891b0a874e080df6e77db1f1d0c9
Nguyễn Ngọc Long <51698746+ZNLong2203@users.noreply.github.com>
2024-05-29 20:00:58 +0700

    add product

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ class Database {
             .then(() => {
                 console.log("Database connection successful")
                 app.listen(process.env.PORT || 3000, () => {
-                    console.log("Server is running on port 3000")
+                    console.log(`Server is running on port ${process.env.PORT || 3000}`)
                 })
             })
             .catch(err => {
--- server/services/product.js
+++ server/services/product.js
@@ -6,14 +6,14 @@ class ProductFactory {
         this.product = product
     }
 
-    createProduct() {
+    async createProduct() {
         switch(this.product.product_type) {
             case 'Electronics':
-                return new Electronics_Class(this.product)
+                return await new Electronics_Class(this.product).createProduct()
             case 'Clothings':
-                return new Clothing_Class(this.product)
+                return await new Clothings_Class(this.product).createProduct()
             case 'Furnitures':
-                return new Furniture_Class(this.product)
+                return await new Furnitures_Class(this.product).createProduct()
             default:
                 throw new Error('Invalid product type')
         }
@@ -21,21 +21,14 @@ class ProductFactory {
 }
 
 // Define base class for product
-class Product_Class {
-    constructor({product_name, product_image, product_description, product_price, product_quantity, product_type, product_shop, product_attributes}) {
-        this.product_name = product_name
-        this.product_image = product_image
-        this.product_description = product_description
-        this.product_price = product_price
-        this.product_quantity = product_quantity
-        this.product_type = product_type
-        this.product_shop = product_shop
-        this.product_attributes = product_attributes
+class Products_Class {
+    constructor(product) {
+        this.product = product
     }
 
     async createProduct() {
         try {
-            const product = new Products(this)
+            const product = new Products(this.product)
             await product.save()
             return product
         } catch(err) {
@@ -45,13 +38,13 @@ class Product_Class {
 }
 
 // Define child class for electronics
-class Electronics_Class extends Product_Class {
+class Electronics_Class extends Products_Class {
     async createProduct() {
         try {
-            const newElectronics = new Electronics(this.product_attributes)
+            const newElectronics = new Electronics(this.product.product_attributes)
             await newElectronics.save()
             
-            const newProduct = await super.createProduct(this)
+            const newProduct = await super.createProduct()
             return newProduct
         } catch (err) {
             throw new Error(err)
@@ -60,13 +53,13 @@ class Electronics_Class extends Product_Class {
 }
 
 // Define child class for clothing
-class Clothing_Class extends Product_Class {
+class Clothings_Class extends Products_Class {
     async createProduct() {
         try {
-            const newClothing = new Clothings(this.product_attributes)
+            const newClothing = new Clothings(this.product.product_attributes)
             await newClothing.save()
 
-            const newProduct = await super.createProduct(this)
+            const newProduct = await super.createProduct()
             return newProduct
         } catch (err) {
             throw new Error(err)
@@ -75,13 +68,13 @@ class Clothing_Class extends Product_Class {
 }
 
 // Define child class for furniture
-class Furniture_Class extends Product_Class {
+class Furnitures_Class extends Products_Class {
     async createProduct() {
         try {
-            const newFurniture = new Furnitures(this.product_attributes)
+            const newFurniture = new Furnitures(this.product.product_attributes)
             await newFurniture.save()
 
-            const newProduct = await super.createProduct(this)
+            const newProduct = await super.createProduct()
             return newProduct
         } catch (err) {
             throw new Error(err)
